0f15cc2662ebecd727c669d381b2dceca1fac0c7
Nemanja Milivojevic <nemanjamil@gmail.com>
2016-08-02 11:08:35 +0200

    Pull List

--- app/src/main/java/rs/projekat/enrg/energymeters/GlavnaAktivnost.java
+++ app/src/main/java/rs/projekat/enrg/energymeters/GlavnaAktivnost.java
@@ -5,8 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import rs.projekat.enrg.energymeters.common.EndPoints;
+import rs.projekat.enrg.energymeters.model.ListaSenzora;
+import rs.projekat.enrg.energymeters.network.PullWebContent;
+import rs.projekat.enrg.energymeters.network.VolleySingleton;
+import rs.projekat.enrg.energymeters.network.WebRequestCallbackInterface;
 
 public class GlavnaAktivnost extends AppCompatActivity {
+
+    private VolleySingleton mVolleySingleton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,46 @@ public class GlavnaAktivnost extends AppCompatActivity {
             }
         });
 
-        // komentar
+        // prvo pravimo objekata koji ce nesto da radi
+        // napravi nam objekat za voley // a volay sluzi da pravi webRequest
+        mVolleySingleton = VolleySingleton.getsInstance(this);
+
+        // prazna gajba piva ListaSenzora  // mi smo predefinisali gajbu kako izgleda
+        // sta gadjam sa praznom gajbom. Trazim Lav ili Koka kolu .... i to mi je URLLISTASENZORA
+        // mVolleySingleton - je prodavacica koja donosi flajke, ali ne stavlja u gajbu
+        // u gajbu trpa flajke PullWebContent
+        PullWebContent<ListaSenzora> webcontent = new PullWebContent<ListaSenzora>(ListaSenzora.class, EndPoints.URLLISTASENZORA, mVolleySingleton);
+
+        // sada odredjumeo sa se radi sa punom gajbom pića
+        // i sta se radi ako pukne flasa ili nesto ne radi
+
+        webcontent.setCallbackListener(new WebRequestCallbackInterface<ListaSenzora>() {
+            // sta ako je uspesno
+            @Override
+            public void webRequestSuccess(boolean success, ListaSenzora listaSenzora) {
+                if (success) {
+                    // ako ima podataka
+                    Toast.makeText(GlavnaAktivnost.this,"Sve je ok",Toast.LENGTH_LONG).show();
+
+                } else {
+                    // ako nema nista
+                    Toast.makeText(GlavnaAktivnost.this,"Nema podataka u JSON",Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            // ako je fail
+            @Override
+            public void webRequestError(String error) {
+                Toast.makeText(GlavnaAktivnost.this,"Nesto je prslo",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        // Sada iniciramo povlacenje
+        webcontent.pullList();
+
+
 
     }
 
--- app/src/main/java/rs/projekat/enrg/energymeters/common/EndPoints.java
+++ app/src/main/java/rs/projekat/enrg/energymeters/common/EndPoints.java
@@ -4,4 +4,7 @@ package rs.projekat.enrg.energymeters.common;
  * Created by 1 on 8/2/2016.
  */
 public class EndPoints {
+    public static final String URLLISTASENZORA = "http://direktnoizbaste.rs/parametri.php?action=lab011listaSenzora";
+    public static final String URLPODACIPOSENZORU = "direktnoizbaste.rs/parametri.php?action=lab011Out&IdSmetersIdchar=%1$s&ipAdresa=%2$s";  // $s da je string
+
 }
